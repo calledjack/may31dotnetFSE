e21bbb9c2eec268e2d65df4a6c29e709c5ea1ef5
calledjack <gjack.ng@gmail.com>
2021-06-01 10:43:06 +0800

    Second day

---
+++ Day2/Array/Array/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = Convert.ToInt32(Console.ReadLine());
+            string[] words = new string[length];
+            int counter = 0;
+            string strChoice = "";
+            do
+            {
+                strChoice = Console.ReadLine();
+                words[counter] = strChoice;
+                counter++;
+            } while (strChoice != "No" && counter <= words.Length);
+            Console.WriteLine("You have entered the following");
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine(words[i]);
+            }
+
+            }
+    }
+}
