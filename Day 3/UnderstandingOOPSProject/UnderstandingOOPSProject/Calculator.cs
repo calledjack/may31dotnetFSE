2f76bda7dabe29f987a385001ae7db5d2665f46a
calledjack <gjack.ng@gmail.com>
2021-06-02 11:00:29 +0800

    Third day

---
+++ Day 3/UnderstandingOOPSProject/UnderstandingOOPSProject/Calculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace UnderstandingOOPSProject
+{
+    public class Calculator
+    {
+        int num1, num2;
+
+        void TakeInstance()
+        {
+            Console.WriteLine("Please Enter First Number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter Second Number");
+            num2 = Convert.ToInt32(Console.ReadLine());
+        }
+
+        void PrintResult(int result)
+        {
+            Console.WriteLine($"The result is {result}");
+        }
+
+
+        public void Add()
+        {
+            TakeInstance();
+            int addition = num1 + num2;
+            PrintResult(addition);
+        }
+        public void Multiply()
+        {
+            TakeInstance();
+            int product = num1 * num2;
+            PrintResult(product);
+        }
+
+        public Calculator()
+        {
+
+        }
+    }
+}
---
+++ Day 3/UnderstandingOOPSProject/UnderstandingOOPSProject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace UnderstandingOOPSProject
+{
+    class Program
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Program()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            calculator.Add();
+            Console.ReadKey();
+        }
+    }
+}
